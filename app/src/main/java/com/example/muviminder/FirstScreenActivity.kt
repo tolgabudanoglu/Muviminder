9b551835c26829012ffe2fc455d9ed84cd1745ef
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-04-17 17:08:19 +0300

    google ile giriş kodalrı yazıldı

---
+++ app/src/main/java/com/example/muviminder/FirstScreenActivity.kt
@@ -0,0 +1,99 @@
+package com.example.muviminder
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_first_screen.*
+
+class FirstScreenActivity: AppCompatActivity() {
+
+
+    lateinit var auth : FirebaseAuth
+    lateinit var googleSignInClient : GoogleSignInClient
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_screen)
+
+
+        btnGoogle.setOnClickListener{
+            signInWithGoogle()
+        }
+
+        btnLoginPage.setOnClickListener{
+            val intent : Intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSignPage.setOnClickListener{
+            val intent : Intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+    private fun signInWithGoogle(){
+        auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        val signIntent = googleSignInClient.signInIntent
+        launcher.launch(signIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            val  task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResult(task)
+        }
+
+
+    }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUı(account)
+            }
+
+        }else{
+            Toast.makeText(this,task.exception.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun updateUı(account: GoogleSignInAccount) {
+
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if (it.isSuccessful){
+                val intent : Intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this,it.exception.toString(), Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/muviminder/LoginActivity.kt
+++ app/src/main/java/com/example/muviminder/LoginActivity.kt
@@ -1,14 +1,23 @@
 package com.example.muviminder
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_first_screen.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 
@@ -17,11 +26,17 @@ class LoginActivity : AppCompatActivity() {
     lateinit var myAuthStateListener: FirebaseAuth.AuthStateListener
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         initMyAuthStateListener()
+
+
+
+
+
 
 
         tvKayıtOlLogin.setOnClickListener {
@@ -92,6 +107,7 @@ class LoginActivity : AppCompatActivity() {
 
 
     }
+
 
     private fun progressBarShow() {
         progressBarLogin.visibility = View.VISIBLE

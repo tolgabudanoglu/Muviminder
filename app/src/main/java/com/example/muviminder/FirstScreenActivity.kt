015c204923f9c794d3d4c7a699b81afb119869e6
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-04-19 16:24:55 +0300

    facebook ile giriş

--- app/src/main/java/com/example/muviminder/FirstScreenActivity.kt
+++ app/src/main/java/com/example/muviminder/FirstScreenActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -18,6 +19,9 @@ import kotlinx.android.synthetic.main.activity_first_screen.*
 class FirstScreenActivity: AppCompatActivity() {
 
 
+
+
+    var callbackManager = CallbackManager.Factory.create()
     lateinit var auth : FirebaseAuth
     lateinit var googleSignInClient : GoogleSignInClient
 
